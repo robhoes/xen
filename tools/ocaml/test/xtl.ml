2b0e32fd6788febaf47f6a5a2c994950f06d5727
Rob Hoes <rob.hoes@citrix.com>
2013-03-19 15:47:46 +0000

    libxc: ocaml: add simple binding for xentoollog (output only).
    These bindings allow ocaml code to receive log message via xentoollog
    but do not support injecting messages into xentoollog from ocaml.
    Receiving log messages from libx{c,l} and forwarding them to ocaml is
    the use case which is needed by the following patches.
    
    Add a simple noddy test case (tools/ocaml/test).
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

---
+++ tools/ocaml/test/xtl.ml
@@ -0,0 +1,19 @@
+open Arg
+open Xentoollog
+  
+let do_test level = 
+  let lgr = Xentoollog.create_stdio_logger ~level:level () in
+  begin
+    Xentoollog.test lgr;
+  end
+
+let () =
+  let debug_level = ref Xentoollog.Info in
+  let speclist = [
+    ("-v", Arg.Unit (fun () -> debug_level := Xentoollog.Debug), "Verbose");
+    ("-q", Arg.Unit (fun () -> debug_level := Xentoollog.Critical), "Quiet");
+  ] in
+  let usage_msg = "usage: xtl [OPTIONS]" in
+  Arg.parse speclist (fun s -> ()) usage_msg;
+
+  do_test !debug_level
