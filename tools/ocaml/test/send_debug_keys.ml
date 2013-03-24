964f07f0cb359f09d64e22d229ebd13eac621762
Rob Hoes <rob.hoes@citrix.com>
2013-03-24 19:32:21 +0000

    libxl: ocaml: implement some simple tests
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

---
+++ tools/ocaml/test/send_debug_keys.ml
@@ -0,0 +1,16 @@
+open Arg
+open Printf
+open Xentoollog
+open Xenlight
+
+let send_keys ctx s = 
+  printf "Sending debug key %s\n" s;
+  Xenlight.send_debug_keys ctx s;
+  ()
+  
+let _ = 
+  let logger = Xentoollog.create_stdio_logger () in
+  let ctx = Xenlight.ctx_alloc logger in
+  Arg.parse [
+  ] (fun s -> send_keys ctx s) "usage: send_debug_keys <keys>"
+
